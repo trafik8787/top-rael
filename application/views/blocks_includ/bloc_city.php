5776561c0cb017f124c937956a8fc1a71f5acc9f
Shtirlic <trafik8787@gmail.com>
2016-01-19 00:31:40 +0200

    fix bags city module

--- application/views/blocks_includ/bloc_city.php
+++ application/views/blocks_includ/bloc_city.php
@@ -5,25 +5,57 @@
  * Date: 17.01.2016
  * Time: 23:37
  */
+
 ?>
+
+<style>
+
+    .w-bloc-right td {
+        /*padding-right: 14%;*/
+    }
+
+    a.w-city-general {
+        font-size: 15px;
+    }
+
+    .w-bloc-right .table>tbody>tr>td {
+        border: none;
+    }
+</style>
+
 <hr>
 <div class="w-bloc-right">
     <h2>По городам</h2>
-    <? foreach ($data['general'] as $row): ?>
+    <table class="table">
+    <? foreach (array_chunk($data['general'], 3) as $row): ?>
 
-        <a href="/city/<?=$row['cityUrl'] ?>"><?=$row['cityName'] ?></a><br>
+        <tr>
+            <td><a class="w-city-general" href="/city/<?=isset($row[0]['cityUrl']) ? $row[0]['cityUrl'] : '' ?>"><?=isset($row[0]['cityName']) ? $row[0]['cityName'] : '' ?></a></td>
+            <td><a class="w-city-general" href="/city/<?=isset($row[1]['cityUrl']) ? $row[1]['cityUrl'] : '' ?>"><?=isset($row[1]['cityName']) ? $row[1]['cityName'] : '' ?></a></td>
+            <td><a class="w-city-general" href="/city/<?=isset($row[2]['cityUrl']) ? $row[2]['cityUrl'] : '' ?>"><?=isset($row[2]['cityName']) ? $row[2]['cityName'] : '' ?></a></td>
+        </tr>
 
     <? endforeach ?>
-
+    </table>
     <!-- Single button -->
     <div class="btn-group" style="margin-top: 10px">
 
         <a href="#"  class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Другие города <span class="caret"></span> </a>
-        <ul class="dropdown-menu">
-            <? foreach ($data['all'] as $row_all): ?>
-                <li><a href="/city/<?=$row_all['cityUrl'] ?>"><?=$row_all['cityName'] ?></a></li>
+
+        <div class="dropdown-menu">
+            <table class="table">
+            <? foreach (array_chunk($data['all'], 3) as $row_all): ?>
+
+                <tr>
+                    <td><a class="w-city-general" href="/city/<?=isset($row_all[0]['cityUrl']) ? $row_all[0]['cityUrl'] : '' ?>"><?=isset($row_all[0]['cityName']) ? $row_all[0]['cityName'] : '' ?></a></td>
+                    <td><a class="w-city-general" href="/city/<?=isset($row_all[1]['cityUrl']) ? $row_all[1]['cityUrl'] : '' ?>"><?=isset($row_all[1]['cityName']) ? $row_all[1]['cityName'] : '' ?></a></td>
+                    <td><a class="w-city-general" href="/city/<?=isset($row_all[2]['cityUrl']) ? $row_all[2]['cityUrl'] : '' ?>"><?=isset($row_all[2]['cityName']) ? $row_all[2]['cityName'] : '' ?></a></td>
+                </tr>
+
             <? endforeach ?>
-        </ul>
+            </table>
+        </div>
+
     </div>
 
 </div>
