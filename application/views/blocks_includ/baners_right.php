7488c4641b4aef4ea999e335959181a4ceccf17a
Shtirlic <trafik8787@gmail.com>
2016-05-23 01:07:02 +0300

    fix reclam google right bloc

--- application/views/blocks_includ/baners_right.php
+++ application/views/blocks_includ/baners_right.php
@@ -40,16 +40,28 @@
         </div>
     </div>
 <?else:?>
-    <div class="discount w-bloc-right">
-        <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-        <!-- Адаптивный -->
-        <ins class="adsbygoogle"
-             style="display:block"
-             data-ad-client="ca-pub-6089615049498543"
-             data-ad-slot="5552405714"
-             data-ad-format="auto"></ins>
-        <script>
-            (adsbygoogle = window.adsbygoogle || []).push({});
-        </script>
+    <div class="discount w-bloc-right text-center">
+
+        <span class="visible-lg">
+
+            <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script> <!-- 300x600 -->
+            <ins class="adsbygoogle"
+                 style="display:inline-block;width:300px;height:600px"
+                 data-ad-client="ca-pub-6089615049498543"
+                 data-ad-slot="2598939313"></ins> <script>
+                (adsbygoogle = window.adsbygoogle || []).push({}); </script>
+
+        </span>
+
+
+
+        <span class="hidden-lg">
+
+            <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script> <!-- Адаптивный -->
+            <ins class="adsbygoogle"
+                 style="display:block" data-ad-client="ca-pub-6089615049498543" data-ad-slot="5552405714" data-ad-format="auto"></ins>
+            <script>
+                (adsbygoogle = window.adsbygoogle || []).push({}); </script>
+        </span>
     </div>
 <?endif?>
