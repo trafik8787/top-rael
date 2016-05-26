132da7ed26216befc792417408f44f33b7e6d65e
Shtirlic <trafik8787@gmail.com>
2016-05-26 10:44:18 +0300

    fix mail

--- application/views/email/mail.php
+++ application/views/email/mail.php
@@ -11,33 +11,33 @@ header('Content-Type: text/html; charset=utf-8');
 <html>
 
 <head>
-
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 
 </head>
 
-<body>
+<body style="background: #ECECEC;padding: 0px;margin: 0px;">
 
-<div style="border:1px solid #e3e1e1;max-width: 600px;margin: 0 auto;width:100%;font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif;font-size:14px;">
-    <div style="border:1px solid #d4d2d2">
-        <div style="border:1px solid #bfbdbd;background:#ffffff;padding:15px 25px;">
-            <div style="display: table;width:100%;">
-                <div style="display: table-cell;width:50%;">
+<div style="max-width: 600px;margin: 0 auto;width:100%;font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif;font-size:14px;">
+    <div style="">
+        <div style="background:#ffffff;padding:15px 25px;">
+            <div style="width:100%;">
+                <div style="display: inline-block;vertical-align: middle;width: 70%;margin-bottom: 10px;">
                     <img src="cid:logo-new.png" width="250" alt="logo">
                 </div>
-                <div style="display: table-cell;width:50%;font-size:12px;color: #a9a9a9;text-align: right;vertical-align: middle;">
+                <div style="display: inline-block;font-size:12px;color: #a9a9a9;text-align: right;vertical-align: middle;margin-bottom: 10px;">
                     Рассылка за <?=Date::rusdate(strtotime(date('Y-m-d')), 'j %MONTH% Y'); ?>
                 </div>
             </div>
             <?if (!empty($article_shift)):?>
-                <h3 style="margin: 20px 0px 10px 0px;padding: 0px;font-size: 22px;">Обзоры</h3>
+                <h3 style="margin: 10px 0px 10px 0px;padding: 0px;font-size: 22px;">Обзоры</h3>
 
                 <a href="http://<?=$_SERVER['HTTP_HOST']?>/article/<?=$article_shift['url']?>" target="_blank" rel="noopener" style="width: 100%;padding:0px;margin:0px;border:0px;">
                     <img src="cid:<?=basename($article_shift['images_article'])?>" style="width: 100%;padding:0px;margin:0px;border:0px;"/>
                 </a>
-                <div style="background: #E6E6E6;padding: 15px 10px;">
-                    <a href="http://<?=$_SERVER['HTTP_HOST']?>/article/<?=$article_shift['url']?>" style="color:#000;font-size:28px;font-weight: bold;" target="_blank" rel="noopener"><?=$article_shift['name']?></a>
-                    <p style="font-size:14px;margin:10px 0px 0px 0px;"> <?=Text::limit_chars(strip_tags($article_shift['content']), 150, null, true)?></p>
+                <div style="background: #666666;padding: 15px 10px;">
+                    <a href="http://<?=$_SERVER['HTTP_HOST']?>/article/<?=$article_shift['url']?>" style="color:#fff;font-size:24px;font-weight: bold;" target="_blank" rel="noopener"><?=$article_shift['name']?></a>
+                    <p style="font-size:14px;margin:10px 0px 0px 0px;color: #fff;"> <?=Text::limit_chars(strip_tags($article_shift['content']), 150, null, true)?></p>
                 </div>
             <?endif?>
 
@@ -51,7 +51,7 @@ header('Content-Type: text/html; charset=utf-8');
                             </a>
                         </div>
                         <div style="">
-                            <a href="http://<?=$_SERVER['HTTP_HOST']?>/article/<?=$artic['url']?>" style="font-size: 20px;color:#000;" target="_blank" rel="noopener">
+                            <a href="http://<?=$_SERVER['HTTP_HOST']?>/article/<?=$artic['url']?>" style="font-size: 18px;color:#000;" target="_blank" rel="noopener">
                                 <strong><?=$artic['name']?></strong>
                             </a>
                             <p style="margin:10px 0;font-size:14px;">
@@ -80,9 +80,9 @@ header('Content-Type: text/html; charset=utf-8');
 
                     <div style="display: table;width:100%;margin: 0px 0px 0px 0px;">
 
-                        <div style="display: table-cell;width:50%;padding-top:20px;">
-                            <div style="height: 400px;border: 1px solid #d8d8d8;box-shadow: rgba(0, 0, 0, 0.3) 0 2px 3px;margin-right: 7.5px;">
-                                <div style="font-size: 25px;line-height: 1.1;color: #e02929;">
+                        <div style="margin: 0px -7.5px;text-align: center;">
+                            <div style="padding-top:20px;width:280px;display: inline-block;vertical-align: top;">
+                                <div style="height: 400px;border: 1px solid #d8d8d8;box-shadow: rgba(0, 0, 0, 0.3) 0 2px 3px;margin: 0px 7.5px;">
                                     <div style="height: 50px;color: #fff;font-size: 80%;background: rgba(0, 0, 0, 0.5);padding: 5px 10px;">
                                         <?=$row_coupons[0]['BusName']?>
                                     </div>
@@ -101,9 +101,9 @@ header('Content-Type: text/html; charset=utf-8');
                         </div>
 
                         <? if (!empty($row_coupons[1])): ?>
-                            <div style="display: table-cell;width:50%;padding-top:20px;">
-                                <div style="height: 400px;border: 1px solid #d8d8d8;box-shadow: rgba(0, 0, 0, 0.3) 0 2px 3px;margin-left: 7.5px;">
-                                    <div style="font-size: 25px;line-height: 1.1;color: #e02929;">
+                            <div style="margin: 0px -7.5px;text-align: center;">
+                                <div style="padding-top:20px;width:280px;display: inline-block;vertical-align: top;">
+                                    <div style="height: 400px;border: 1px solid #d8d8d8;box-shadow: rgba(0, 0, 0, 0.3) 0 2px 3px;margin: 0px 7.5px;">
                                         <div style="height: 50px;color: #fff;font-size: 80%;background: rgba(0, 0, 0, 0.5);padding: 5px 10px;">
                                             <?=$row_coupons[1]['BusName']?>
                                         </div>
@@ -121,7 +121,7 @@ header('Content-Type: text/html; charset=utf-8');
                                 </div>
                             </div>
                         <?else:?>
-                            <div style="display: table-cell;width:50%;padding-top:20px;">
+                            <div style="padding-top:20px;width:280px;display: inline-block;">
                             </div>
                         <?endif?>
 
@@ -142,13 +142,13 @@ header('Content-Type: text/html; charset=utf-8');
 
                 <? foreach ($business as $row_bus): ?>
 
-                    <h3 style="margin: 30px 0px 10px 0px;padding: 0px;font-size: 22px;"><?=$row_bus['CatName']?></h3>
+                    <h3 style="margin: 30px 0px 0px 0px;padding: 0px;display:block;font-size: 22px;"><?=$row_bus['CatName']?></h3>
 
                     <?foreach (Controller_BaseController::convertArrayVievData($row_bus['BusArr']) as $bus_rows):?>
 
-                        <div style="display: table;width:100%;margin: 20px 0px 0px 0px;">
-                            <div style="display: table-cell;width:50%;">
-                                <div style="height: 380px;margin-right: 7.5px;">
+                        <div style="margin: 0px -7.5px;text-align: center;">
+                            <div style="padding-top:20px;width:280px;display: inline-block;text-align: left;vertical-align: top;">
+                                <div style="height: 380px;margin: 0px 7.5px;">
                                     <a href="http://<?=$_SERVER['HTTP_HOST']?>/business/<?=$bus_rows[0]['url']?>" style="display:block;overflow: hidden;" target="_blank" rel="noopener">
                                         <img src="cid:<?=basename($bus_rows[0]['home_busines_foto'])?>" style="width:100%;height:auto;">
                                     </a>
@@ -165,8 +165,8 @@ header('Content-Type: text/html; charset=utf-8');
                             </div>
 
                             <?if (!empty($bus_rows[1])):?>
-                                <div style="display: table-cell;width:50%;">
-                                    <div style="height: 380px;margin-left: 7.5px;">
+                                <div style="padding-top:20px;width:280px;display: inline-block;text-align: left;vertical-align: top;">
+                                    <div style="height: 380px;margin: 0px 7.5px;">
                                         <a href="http://<?=$_SERVER['HTTP_HOST']?>/business/<?=$bus_rows[1]['url']?>" style="display:block;overflow: hidden;" target="_blank" rel="noopener">
                                             <img src="cid:<?=basename($bus_rows[1]['home_busines_foto'])?>" style="width:100%;height:auto;">
                                         </a>
@@ -176,10 +176,14 @@ header('Content-Type: text/html; charset=utf-8');
                                         <div style="font-size:14px;color:#000;margin-top: 10px;" >
                                             <strong><?=$bus_rows[1]['CityName']?>. <?=$bus_rows[1]['address']?></strong>
                                         </div>
-                                        <div style="font-size:14px;color:#000;margin-top: 10px;" >
+                                        <div style="font-size:14px;color:#000;margin-top: 10px;">
                                             <?=Text::limit_chars(strip_tags($bus_rows[0]['info']), 150, null, true)?>
                                         </div>
                                     </div>
+                                </div>
+                            <?else:?>
+                                <div style="padding-top:20px;width:280px;display: inline-block;">
+                                    <!-- заглушка -->
                                 </div>
                             <?endif?>
                         </div>
@@ -195,8 +199,8 @@ header('Content-Type: text/html; charset=utf-8');
             <div style="margin: 10px -25px -15px -25px">
                 <div style="background: #0389ae;padding:30px 25px;font-size: 12px;color: #fff;text-align: center">
                     Вы получили это письмо, потому что выразили свое согласие получать новости TopIsrael.ru. Если вы не хотите получать рассылку, <a href="http://<?=$_SERVER['HTTP_HOST']?>/unsubscribe" style="color: #ffffff;" target="_blank" rel="noopener">можете отписаться.</a>
-                    <br><br>
-                    Это автоматическое письмо, пожалуйста, не отвечайте на него. Если у вас есть вопросы или пожелания, <a href="http://<?=$_SERVER['HTTP_HOST']?>/contacts" style="color: #ffffff;text-decoration: underline;" target="_blank" rel="noopener">пишите или звоните нам.</a>
+                    <br>
+                    <br>Это автоматическое письмо, пожалуйста, не отвечайте на него. Если у вас есть вопросы или пожелания, <a href="http://<?=$_SERVER['HTTP_HOST']?>/contacts" style="color: #ffffff;text-decoration: underline;" target="_blank" rel="noopener">пишите или звоните нам.</a>
                 </div>
             </div>
 
