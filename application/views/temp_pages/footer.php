aec7277c09d1dc36071f6d5364aa4a595a6edac4
Shtirlic <trafik8787@gmail.com>
2016-07-03 16:51:15 +0300

    fix bags

--- application/views/temp_pages/footer.php
+++ application/views/temp_pages/footer.php
@@ -149,19 +149,6 @@
 
 
 
-    <script>
-        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-                (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-        })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-        ga('create', 'UA-69897111-1', 'auto');
-        ga('send', 'pageview');
-        setTimeout("ga('send', 'event', 'read', '30_seconds')", 30000);
-
-
-    </script>
-
 
     <!-- Yandex.Metrika counter -->
     <script type="text/javascript">
